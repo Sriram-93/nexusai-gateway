79af440d55ef77f20fddac60b799146042d99f39
Sriram-93 <sriramknr63@gmail.com>
2026-06-11 14:15:10 +0530

    initial project setup

---
+++ src/main/java/com/llm/nexusai_gateway/NexusaiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.llm.nexusai_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusaiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusaiGatewayApplication.class, args);
+	}
+
+}
