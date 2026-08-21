5d1a85bf341d7d23cbacdbd006eeb457f25a812d
Sriram-93 <sriramknr63@gmail.com>
2026-08-21 15:13:45 +0530

    fix: explicit port binding to prevent node and spring boot collision

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Start Spring Boot in the background on port 8081
-export server_port=8081
-java -Xmx300m -XX:MaxMetaspaceSize=128m -XX:+UseSerialGC -Djava.security.egd=file:/dev/./urandom -jar app.jar &
+java -Xmx300m -XX:MaxMetaspaceSize=128m -XX:+UseSerialGC -Djava.security.egd=file:/dev/./urandom -jar app.jar --server.port=8081 &
 
 # Start Nitro Node.js Server in the foreground on port 8080
 export PORT=8080
